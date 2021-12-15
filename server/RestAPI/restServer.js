ce4ad185f88513e0f9d7a98d93967d97435f604c
hiyee-gj <gyeongjae.h.dev@gmail.com>
2021-12-15 15:15:55 +0900

    test commit.

--- server/RestAPI/restServer.js
+++ server/RestAPI/restServer.js
@@ -18,8 +18,10 @@ http.createServer(async (req, res) => {
         const data = await fs.readFile('./comment.html');
         res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8'});
         return res.end(data);
-      } 'asdfasdfdsafsafasdfdas'
-      'asdfddddddddddafsadfdasd'
+      } else if (req.url === '/comment') {
+        res.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' });
+        return res.end(JSON.stringify(users));
+      }
       // /도 /about도 /users도 아니면
       try {
         const data = await fs.readFile(`.${req.url}`);
