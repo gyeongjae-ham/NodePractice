2e4cab4ebce545e235d67f1622be500d0d4e4192
hiyee-gj <gyeongjae.h.dev@gmail.com>
2021-12-15 18:17:33 +0900

    Added route with wildcard.

--- publish/app.js
+++ publish/app.js
@@ -1,16 +1,18 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
+app.use((req, res, next) => {
+    console.log('모든 요청에 실행하고 싶어요');
+    next();
+});
 app.get('/', (req, res) => {
-    res.send('hello express');
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
-
-app.post('/', (req, res) => {
-    res.send('hello express');
+app.get('/category/:name', (req, res) => {
+    res.send(`hello ${res.params.name}`);
 });
-
 app.get('/about', (req, res) => {
     res.send('hello express');
 });
