31f6924236cf8957cf967e166f2895425f5f5f8c
hiyee-gj <gyeongjae.h.dev@gmail.com>
2021-12-15 11:09:53 +0900

    Added thread pool.

---
+++ threadpool.js
@@ -0,0 +1,32 @@
+const crypto = require('crypto');
+
+const pass = 'pass';
+const salt = 'salt';
+const start = Date.now();
+
+crypto.pbkdf2(pass, salt, 1_000_000, 128, 'sha512', () => {
+    console.log('1', Date.now() - start);
+});
+crypto.pbkdf2(pass, salt, 1_000_000, 128, 'sha512', () => {
+    console.log('2', Date.now() - start);
+});
+crypto.pbkdf2(pass, salt, 1_000_000, 128, 'sha512', () => {
+    console.log('3', Date.now() - start);
+});
+crypto.pbkdf2(pass, salt, 1_000_000, 128, 'sha512', () => {
+    console.log('4', Date.now() - start);
+});
+crypto.pbkdf2(pass, salt, 1_000_000, 128, 'sha512', () => {
+    console.log('5', Date.now() - start);
+});
+crypto.pbkdf2(pass, salt, 1_000_000, 128, 'sha512', () => {
+    console.log('6', Date.now() - start);
+});
+crypto.pbkdf2(pass, salt, 1_000_000, 128, 'sha512', () => {
+    console.log('7', Date.now() - start);
+});
+crypto.pbkdf2(pass, salt, 1_000_000, 128, 'sha512', () => {
+    console.log('8', Date.now() - start);
+});
+
+// 노드의 기본 스레드 풀을 확인하기 위한 코드 기본으로 4개인 것을 확인할 수 있다
